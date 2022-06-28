d030c8c0dfd26b05623682f077fb47aad64ae138
Barış Kodaş <40144559+bariskodas@users.noreply.github.com>
2022-06-28 08:22:46 +0300

    added new features

--- lib/views/product_detail/product_detail_view.dart
+++ lib/views/product_detail/product_detail_view.dart
@@ -9,133 +9,6 @@ import 'package:flutter/material.dart';
 import 'widgets/color_option.dart';
 import 'widgets/product_cover_widget.dart';
 
-// ignore: must_be_immutable
-// class ProductDetailPage extends ConsumerWidget {
-//   ProductModel product;
-//   ProductDetailPage({Key? key, required this.product}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final double _height = MediaQuery.of(context).size.height;
-//     bool isSelected = false;
-//     String selectedColor;
-
-//     return Scaffold(
-//       body: SingleChildScrollView(
-//         child: SizedBox(
-//           width: double.maxFinite,
-//           height: _height < 900 ? _height * 1.1 : _height,
-//           child: Stack(
-//             children: [
-//               // cover
-//               ProductCoverWidget(imageUrl: product.image.toString()),
-//               // product details
-//               Positioned(
-//                 top: 380,
-//                 left: 10,
-//                 right: 10,
-//                 child: SingleChildScrollView(
-//                   child: Padding(
-//                     padding: Constants.hPadding16,
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         // product name
-//                         MediumTitleText(
-//                             text: product.title.toString(),
-//                             size: 18,
-//                             fontWeight: FontWeight.w900),
-//                         // rating
-//                         Row(
-//                             mainAxisAlignment: MainAxisAlignment.start,
-//                             children: [
-//                               Icon(Icons.star_rate_rounded,
-//                                   color: Constants.yellowColor, size: 36),
-//                               MediumTitleText(
-//                                   text: product.rating!.rate.toString()),
-//                               const SizedBox(width: 20),
-//                               MediumTitleText(
-//                                   text: "(${product.rating!.count} Reviews)",
-//                                   size: 14,
-//                                   fontWeight: FontWeight.w500)
-//                             ]),
-//                         const SizedBox(height: 10),
-//                         // price and stock
-//                         Row(
-//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                             children: [
-//                               MediumTitleText(
-//                                   text: "\$" + product.price.toString(),
-//                                   size: 24),
-//                               MediumTitleText(
-//                                   text: "Avaliable in stock", size: 12),
-//                             ]),
-//                         const SizedBox(height: 20),
-//                         // Colors
-//                         Padding(
-//                           padding: Constants.vPadding8,
-//                           child: Row(
-//                             mainAxisAlignment: MainAxisAlignment.center,
-//                             children: [
-//                            //   Radio(value: "", groupValue: "color", onChanged: (String? value){},),
-//                             //  Radio(value: "", groupValue: "color", onChanged: (String? value){}, fillColor: MaterialStateProperty.all(Colors.red)),
-//                               GestureDetector(
-//                                 onTap: () {
-
-//                                 },
-//                                 child: Container(
-//                                   height: 30,
-//                                   width: 30,
-//                                   decoration: BoxDecoration(
-//                                       shape: BoxShape.circle,
-//                                       border:
-//                                           Border.all(color: isSelected ? Constants.darkGreyColor : Colors.transparent)),
-//                                           child: Padding(
-//                                             padding: Constants.allPadding2,
-//                                             child: Container(decoration: BoxDecoration(shape: BoxShape.circle, color: Constants.purpleColor),),
-//                                           ),
-//                                 ),
-//                               ),
-//                               Container(
-//                                 height: 30,
-//                                 width: 30,
-//                                 decoration: BoxDecoration(
-//                                     shape: BoxShape.circle,
-//                                     border:
-//                                         Border.all(color: isSelected ? Constants.darkGreyColor : Colors.transparent)),
-//                                         child: Padding(
-//                                           padding: Constants.allPadding2,
-//                                           child: Container(decoration: BoxDecoration(shape: BoxShape.circle, color: Constants.deepOrangeColor),),
-//                                         ),
-//                               )
-//                             ],
-//                           ),
-//                         ),
-//                         // About
-//                         MediumTitleText(
-//                             text: "About", fontWeight: FontWeight.w500),
-//                         Text(product.description.toString(),
-//                             overflow: TextOverflow.visible),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//       floatingActionButton: LargeButton(
-//           text: "Add to Cart",
-//           onPressed: () {
-//             ref.read(cartRiverpod).addToProductList(product);
-//           }),
-//       floatingActionButtonLocation:
-//           FloatingActionButtonLocation.miniCenterFloat,
-//     );
-//   }
-// }
-
 class ProductDetailPage extends ConsumerStatefulWidget {
   ProductModel product;
   ProductDetailPage({Key? key, required this.product}) : super(key: key);
@@ -210,12 +83,10 @@ class _ProductDetailPageState extends ConsumerState<ProductDetailPage> {
                             children: [
                               for (var item in ColorModel.colorList)
                                 GestureDetector(
-                                  onTap: () {
-                                    selectedColor = item.colorName;
-                                    setState(() {
-                                      
-                                    });
-                                  },
+                                    onTap: () {
+                                      selectedColor = item.colorName;
+                                      setState(() {});
+                                    },
                                     child: ColorOption(
                                         colorName: item.colorName,
                                         color: item.color,
@@ -240,7 +111,9 @@ class _ProductDetailPageState extends ConsumerState<ProductDetailPage> {
       floatingActionButton: LargeButton(
           text: "Add to Cart",
           onPressed: () {
-            ref.read(cartRiverpod).addToProductList(widget.product, selectedColor);
+            ref
+                .read(cartRiverpod)
+                .addToProductList(widget.product, selectedColor);
           }),
       floatingActionButtonLocation:
           FloatingActionButtonLocation.miniCenterFloat,
